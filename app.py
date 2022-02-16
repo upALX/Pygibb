102b90c13bec30a843ba85e8967718fc4ce8d864
Alexandre <alexandreaddres@gmail.com>
2022-02-15 23:40:15 -0300

    Adding a feature to get words in archive

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
+import random
+
 print('Welcome to the Pygibb!')
+
+arquive_words = open("words.txt", "r")
+words = []
+
+for word in arquive_words:
+    word = word.strip()
+    words.append(word)
+
+arquive_words.close()
+
+number_words = random.randrange(0, len(words))
+
+
 
 gybbed_user = False
 right_kick_word = False
 errors = 0
-secret_word = 'league'.upper()
+secret_word = words[number_words].upper()
 right_letters = ["_" for letter in secret_word]
 
 print(right_letters)
 
 while(not gybbed_user and not right_kick_word):
     kick_user = input('Hi, please insert a word: ').upper().strip()
+
+    print(right_letters)
 
     if(kick_user in secret_word):
         position_letter = 0
